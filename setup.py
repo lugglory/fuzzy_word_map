0f518f19aea9ed38d84ae9614f950f31c180d67e
Hyonyoung Park <lugglory@gmail.com>
2025-04-29 17:46:26 +0900

    pypi 업로드 관련 수정

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='fuzzy_word_map',
-    version='0.0',
+    version='0.0.0',
     description='여러 방법을 혼합하여 효율적으로 유사 단어를 찾는 기능 제공',
     author='Hyonyoung Park',
     author_email='lugglory@gmail.com',
-    url='https://github.com/lugglory/fuzzy_word_finder',
+    url='https://github.com/lugglory/fuzzy_word_map',
     packages=find_packages(),
     install_requires=['python-Levenshtein', 'jamo'],
     classifiers=[
@@ -14,4 +14,7 @@ setup(
         'License :: OSI Approved :: MIT License',
     ],
     license='MIT',
+    python_requires=">=3.7",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
 )
